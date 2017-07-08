9873af356e93e32ce67559334a7b2e9d7148d4cf
Alexandr Tsimbaluk <alexandr.tsimbaluk@gmail.com>
2017-07-09 02:13:11 +0300

    вынес меню админа в шаблон

---
+++ src/layouts/nav-player.php
@@ -0,0 +1,9 @@
+<nav class="nav nav-admin">
+	<ul>
+        <!-- <li class="adminItem"><a class="toPlayer" href="">A</a></li> -->
+		<li class="adminItem"><a class="toAdmin" href="">Admin</a></li>
+		<li class="adminItem"><a class="clearLocalStorage" href="">Reset</a></li>
+		<li class="adminItem"><a class="clearUniqHash" href="">Clear U</a></li>
+		<li class="adminItem"><a class="runSql" href="/sql/db_queries.php">SQL</a></li>
+	</ul>
+</nav>
