132c61ad7af34b7a47f3f3172972393b92ac9e20
Alexandr Tsimbaluk <aleksandr.tsimbaluk@gmail.com>
2018-05-05 03:59:11 +0300

    поправил пути в php

--- src/index.php
+++ src/index.php
@@ -3,11 +3,11 @@
 	session_start();
 
 	
-	include_once 'layouts/head.php';
+	include_once './layouts/head.php';
 	
-	include_once 'layouts/player.php';
-	include_once 'layouts/devel.php';
-	// include_once 'layouts/fractals.php';
+	include_once './layouts/player.php';
+	include_once './layouts/devel.php';
+	// include_once './layouts/fractals.php';
 	
-	include_once 'layouts/footer.php';
+	include_once './layouts/footer.php';
 ?>	
