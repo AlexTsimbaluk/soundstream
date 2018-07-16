1296bbb828319db120537c9a32f7f34ffd6f3bba
Alexandr Tsimbaluk <aleksandr.tsimbaluk@gmail.com>
2018-07-17 01:51:17 +0300

    статистика

--- src/layouts/footer.php
+++ src/layouts/footer.php
@@ -25,10 +25,10 @@
 	<!-- <script src="js/app.js"></script> -->
 	
 	<!-- <script src="js/user.js"></script> -->
-	<!-- <script src="js/visits.js"></script> -->
 	<script src="js/admin.js"></script>
 
 	<script src="js/player.js"></script>
+	<script src="js/visits.js"></script>
 	<!-- <script src="js/fractals.js"></script> -->
 	<!-- <script src="js/fractals-advanced.js"></script> -->
 
